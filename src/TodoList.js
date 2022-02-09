cdeba34b7eb7ae9c8503ee16d9b914194f385413
No Future <faizanahmed3742@gmail.com>
2022-02-09 06:03:21 +0500

    first commit

---
+++ src/TodoList.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
+import { useState } from "react";
+import List from "./list";
+
+export default function TodoList() {
+  const [item, setItem] = useState("");
+  const [newItem, setNewItem] = useState([]);
+  const itemAdd = (e) => {
+    setItem(e.target.value);
+  };
+  const listOfData = () => {
+    setNewItem((preValue) => {
+      return [...preValue, item];
+    });
+    setItem(" ");
+  };
+  const deleteItems=(id)=>{
+    console.log("deleted");
+    setNewItem((newItem) =>{
+       return newItem.filter((arr, index)=>{
+        return index !== id
+    });
+});
+  }
+
+
+
+  return (
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <br />
+          <h1> TodoList </h1>
+          <br />
+
+          <input
+            type="text"
+            value={item}
+            placeholder="Add Items"
+            onChange={itemAdd}
+          />
+
+          <Button className="newBtn" onClick={listOfData}>
+            <AddIcon />
+          </Button>
+          <br />
+          <ol>
+            {newItem.map((val, index) => {
+              return( 
+              <List 
+              key={index} 
+              text={val} 
+              id={index}
+              onSelect={deleteItems}
+              />);
+            })}
+          </ol>
+          <br />
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/list.js
@@ -0,0 +1,15 @@
+import React from "react";
+import HighlightOffIcon from "@material-ui/icons/HighlightOff";
+export default function List(props) {
+  return (
+    <div className="todo_style">
+      <span>
+        <HighlightOffIcon 
+        className="deleteIcon" 
+        onClick={ ()=>{
+            props.onSelect(props.id); }}/>
+      </span>
+      <li>{props.text}</li>
+    </div>
+  );
+}
